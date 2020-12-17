ce2ca6565cd3e3ce5e987dad7a79416cfd8f0bc4
rainydaysgameplays <rainydaysgameplays@gmail.com>
2020-12-16 21:23:02 -0600

    button clickable

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log(`consol is working`);
+
+const button = document.querySelectorAll(`.button`);
+
+
+const buttonClick = function(){
+    console.log(`button Clicked`)
+};
+
+button.forEach(buttons => {
+    buttons.addEventListener('click', buttonClick)
+})
