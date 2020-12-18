5f23931e4cb73f9d0c876e8e730f92f4e73bd8da
rainydaysgameplays <rainydaysgameplays@gmail.com>
2020-12-17 23:37:24 -0600

    compare base done

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ const topLeft = document.querySelector(`#topLeft`);
 const botRight = document.querySelector(`#botRight`);
 const botLeft = document.querySelector(`#botLeft`);
 const botCenter = document.querySelector(`#botCenter`);
+const currentScore = document.querySelector('#currentScore');
+const startButton = document.querySelector('#startButton');
+
+startButton.addEventListener('click', computerChoice);
 
 async function sleep(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-let computer = [0,1,2,3,4,0,1,2,3,4]
+
+let computer = []
 
 async function computerChoice (){
     let randomNum = Math.floor(Math.random()*5);
     computer.push(randomNum);
+    currentScore.innerText = computer.length;
+
     
-
-
     for(let i=0; i< computer.length; i++){
         if (computer[i] == 0){
             await sleep(250);
@@ -24,7 +29,6 @@ async function computerChoice (){
             await sleep(1000);
             botCenter.style.animationPlayState = "paused";
             await sleep(250);
-            
         }
         else if (computer[i] == 1){
             await sleep(250);
@@ -56,18 +60,61 @@ async function computerChoice (){
             
         }
     }
-    
     console.log(`the computer array is ${computer}`)
 }
 
+let user = [];
 
 async function buttonClick(event){
-    event.preventDefault();
     
-    computerChoice()
-    console.log(`button Clicked ${event.target.value}`)
+    let userClick = event.target.value
+    user.push(userClick);
+   
+    if (userClick == 0){
+        botCenter.style.animationPlayState = "running";
+        await sleep(1000);
+        botCenter.style.animationPlayState = "paused";
+        
+    }
+    else if (userClick == 1){
+        topLeft.style.animationPlayState = "running";
+        await sleep(1000);
+        topLeft.style.animationPlayState = "paused";
+    }
+    else if (userClick == 2){
+        topRight.style.animationPlayState = "running";
+        await sleep(1000);
+        topRight.style.animationPlayState = "paused";
+    }
+    else if (userClick == 3){
+        botRight.style.animationPlayState = "running";
+        await sleep(1000);
+        botRight.style.animationPlayState = "paused";
+    }
+    else if (userClick== 4){
+        botLeft.style.animationPlayState = "running";
+        await sleep(1000);
+        botLeft.style.animationPlayState = "paused";
+    }
+
+    console.log(`button Clicked ${user}`)
+
+    if(user.length == computer.length && computer.length !== 0){
+        await compareArray();
+        user = []
+    }
 };
+
 
 button.forEach(buttons => {
     buttons.addEventListener('click', buttonClick)
 })
+
+async function compareArray(){
+    for (let i = 0; i <= computer.length; i++){
+        if(user[i] != computer[i]){
+            console.log(`you messed up`)
+        }
+    }
+}
+
