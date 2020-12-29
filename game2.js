1a5ae7141cfc3c615346d2a9fecc4828ddefaf78
rainydaysgameplays <rainydaysgameplays@gmail.com>
2020-12-28 18:03:30 -0600

    game2 started

--- game2.js
+++ game2.js
@@ -1,1 +1,4 @@
-// project 1 flash card game
+// project 1 flash card game, you know the matching 
+// one, where you match the identicle cards
+// could use the rick and morty api to pull from
+// the images so the player need to match the images
--- game4.js
+++ game4.js
@@ -1,1 +1,2 @@
 //project 1 Spaceman/hangmaan
+// use the final space api and pull from the quotes
